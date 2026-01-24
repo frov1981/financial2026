a3b05d0c52180510b0c80c954a65ee80272b3b34
Fernando Ortega <33201689+frov1981@users.noreply.github.com>
2026-01-24 08:18:52 -0500

    Mejora en JS, distribuido correctamente en los index.ejs

--- src/public/js/helpers/amount-helper.js
+++ src/public/js/helpers/amount-helper.js
@@ -8,3 +8,13 @@ function amountBox(value) {
     </div>
   `
 }
+
+function numberBox(value) {
+  const number = Number(value || 0).toFixed(0)
+
+  return `
+    <div class="number-box">
+      <span class="number-value">${number}</span>
+    </div>
+  `
+}
--- src/public/js/helpers/number-helper.js
+++
@@ -1,9 +0,0 @@
-function numberBox(value) {
-  const number = Number(value || 0).toFixed(0)
-
-  return `
-    <div class="number-box">
-      <span class="number-value">${number}</span>
-    </div>
-  `
-}
