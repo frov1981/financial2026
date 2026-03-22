7273c62f138670d03fd15e95e8ca360d6e829ba4
Fernando Ortega <33201689+frov1981@users.noreply.github.com>
2026-03-22 12:14:58 -0500

    fixed: delete console.log

--- src/controllers/transaction/transaction.controller.ts
+++ src/controllers/transaction/transaction.controller.ts
@@ -113,7 +113,6 @@ export const apiForGettingTransactions: RequestHandler = async (req: Request, re
       .getManyAndCount()
 
     logger.debug(`${apiForGettingTransactions.name}-Transactions found: [${items.length}], Total: [${total}], Page: [${page}], Limit: [${limit}], Search: [${search}], Category ID: [${category_id}]`)
-    logger.debug('items transaction. ', items)
     res.json({ items, total, page, limit, category_id: category_id })
   } catch (error) {
     logger.error(`${apiForGettingTransactions.name}-Error. `, error)
